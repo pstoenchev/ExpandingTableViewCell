defc6d64ca8c3ad8d690db880e054c19b818f579
Petar Stoenchev <peter.stoenchev@abv.bg>
2020-12-03 15:00:09 +0200

    Initial Commit

---
+++ ExpandingTableViewCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExpandingTableViewCell
+//
+//  Created by Petar Stoenchev on 3.12.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
