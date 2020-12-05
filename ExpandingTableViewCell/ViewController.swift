c4bb918a9a7a7a1d995b6b90c7b7ac070ed91407
Petar Stoenchev <peter.stoenchev@abv.bg>
2020-12-05 17:01:40 +0200

    add new file

--- ExpandingTableViewCell/ViewController.swift
+++ ExpandingTableViewCell/ViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+final  class ViewController: UITableViewController {
+    
+    private var dateCellExpanded: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     //   tableView.tableFooterView = UIView()
+    }
+}
+extension ViewController {
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+      //  if indexPath.row == 0 {
+            if dateCellExpanded {
+                dateCellExpanded = false
+            } else {
+                dateCellExpanded = true
+            }
+            tableView.beginUpdates()
+            tableView.endUpdates()
+       // }
     }
 
-
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+   //    if indexPath.row == 0 {
+            if dateCellExpanded {
+                return 250
+            } else {
+                return 50
+            }
+      //  }
+        return 50
+    }
+    
 }
 
